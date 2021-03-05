b924b615537f5ad0a9d98e5a315836838c3637c0
Dmitriy Pronin <32202850+DmitryPronin@users.noreply.github.com>
2021-03-05 22:29:56 +0300

    factorial v 0.2 - add Recursion and menu

--- Factorial/src/factorial/Main.java
+++ Factorial/src/factorial/Main.java
@@ -5,22 +5,59 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    static BigInteger RecursionFactorial(int digit){
+        BigInteger result = new BigInteger("1");
+        if ((digit == 1) || (digit == 0)){
+            return result;
+        }
+        result = BigInteger.valueOf(digit).multiply(RecursionFactorial(digit-1));
+        return result;
+    }
+
+    static void Factorial() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("ВВедите число для рассчета факториала");
+        System.out.println();
+        System.out.println("Введите число для рассчета факториала");
         try {
             int digitForFactorial = scanner.nextInt();
             if (digitForFactorial < 0) {
                 System.out.println("Факториал числа " + digitForFactorial + " = бесконечность");
             } else {
-                BigInteger result = new BigInteger("1");
-                for (int i = digitForFactorial; i > 0; i--) {
-                    result = result.multiply(new BigInteger(String.valueOf(i)));
-                }
-                System.out.println("Факториал числа " + digitForFactorial + " = " + result);
+                System.out.println(RecursionFactorial(digitForFactorial));
             }
         } catch (InputMismatchException e) {
             System.out.println("Вы ввели не число ");
+        }
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                System.out.println("============Меню=============");
+                System.out.println("1 - Рассчитать факторил");
+                System.out.println("2 - Выход");
+                System.out.println("=============================");
+                System.out.println("Введите пункт меню");
+                Scanner scannerForMenu = new Scanner(System.in);
+                int userAnswer = scannerForMenu.nextInt();
+                switch (userAnswer) {
+                    case 1: {
+                        Factorial();
+                    }break;
+                    case 2: {
+                        System.exit(0);
+                    }break;
+                    default: {
+                        System.out.println("Такого пункта нет в меню");
+                        continue;
+                    }
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Вы ввели не корректное значение для меню");
+            }
+
+
         }
     }
 }
